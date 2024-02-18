8629d8e8197ee8951ab0830192d29abdfb24de07
petrlutkin <lutkinpeter@gmail.com>
2024-02-18 13:36:03 +0300

    Added the ability to move cards. Implemented Deck class

--- main.py
+++ main.py
@@ -13,14 +13,43 @@ SUITS = ['hearts', 'diamonds', 'clubs', 'spades']
 RANKS = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king']
 
 
+class Deck:
+    def __init__(self):
+        self.deck = create_deck()
+        self.cards = self.deck.copy()
+        self.shuffle()
+        self.x, self.y, (self.w, self.h) = 50, 50, CARD_SIZE
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+    def check_pos(self, x, y):
+        if self.x <= x <= self.x + self.w and self.y <= y <= self.y + self.h:
+            return True
+        return False
+
+    def draw_card(self):
+        if self.cards:
+            card = self.cards[0]
+            self.cards = self.cards[1:]
+        else:
+            return None
+        return card
+
+    def spread_cards(self):
+        pass
+
+
 # Функция для создания колоды карт
 def create_deck():
     return [Card(suit, rank, "close") for suit in SUITS for rank in RANKS]
 
 
-# Функция для перемешивания колоды карт
-def shuffle_deck(deck):
-    random.shuffle(deck)
+def is_card_clamped(x, y):
+    for card in deck.deck:
+        if card.check_pos(x, y) and card.is_movable:
+            return card
+    return False
 
 
 # Функция отрисовки игрового поля
@@ -39,47 +68,84 @@ class Card(pygame.sprite.Sprite):
         super().__init__(all_sprites)
         self.suit = suit
         self.rank = rank
+        self.is_movable = True
         self.card_face = load_image("cards", self.suit + "_" + self.rank + ".png", -1)
         self.card_back = load_image("cards", "red_back2.png", -1)
+        self.scale()
         self.status = status
         if self.status == "open":
             self.image = self.card_face
         else:
             self.image = self.card_back
-        self.scale()
         self.rect = self.image.get_rect()
         self.rect.x = 50
         self.rect.y = 50
 
+    #     self._changed = False
+    #
+    # def __setattr__(self, key, value):
+    #     if key != '_changed':
+    #         self._changed = True
+    #     super(Card, self).__setattr__(key, value)
+    #
+    # def is_status_changed(self):
+    #     return self._changed
+
+    def change_status(self, status):
+        if status == "open":
+            self.status = "open"
+            self.image = self.card_face
+        elif status == "close":
+            self.status = "close"
+            self.image = self.card_back
+
     def scale(self):
-        self.image = pygame.transform.scale(self.image, CARD_SIZE)
+        self.card_face = pygame.transform.scale(self.card_face, CARD_SIZE)
+        self.card_back = pygame.transform.scale(self.card_back, CARD_SIZE)
 
     def move(self, x, y):
-        self.rect.x = x
-        self.rect.y = y
+        self.rect = self.rect.move(x, y)
+
+    def check_pos(self, x, y):
+        if self.rect.x <= x <= self.rect.x + self.rect.width and self.rect.y <= y <= self.rect.y + self.rect.height:
+            return True
+        return False
 
 
 if __name__ == '__main__':
     screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
     all_sprites = pygame.sprite.Group()
     clock = pygame.time.Clock()
-    deck = create_deck()
-    shuffle_deck(deck)
+    deck = Deck()
     running = True
     main_menu()
-    draw_background()
     # x = 0
     # y = 0
-    # for card in deck:
+    # for card in deck.deck:
     #     card.move(CARD_SIZE[0] * x, CARD_SIZE[1] * y)
     #     x += 1
     #     if x == 13:
     #         x = 0
     #         y += 1
+    pos = None
+    is_mouse_clamped = False
+    current_card = None
     while running:
+        draw_background()
         for event in pygame.event.get():
-            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == 27):
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                 running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if deck.check_pos(*event.pos):
+                    current_card = deck.draw_card()
+                    is_mouse_clamped = True
+                if current_card := is_card_clamped(*event.pos):
+                    current_card.change_status("open")
+                    is_mouse_clamped = True
+            if event.type == pygame.MOUSEBUTTONUP:
+                is_mouse_clamped = False
+            if event.type == pygame.MOUSEMOTION and is_mouse_clamped:
+                current_card.move(*event.rel)
         all_sprites.draw(screen)
         pygame.display.flip()
         clock.tick(FPS)
